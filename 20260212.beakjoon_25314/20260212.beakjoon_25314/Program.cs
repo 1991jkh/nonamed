63797d88d6bbd5645f81cebbe8f0022769b72b19
1991jkh <demonicjkh@gmail.com>
2026-02-12 18:06:14 +0900

    almost solved

---
+++ 20260212.beakjoon_25314/20260212.beakjoon_25314/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20260212.beakjoon_25314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StringBuilder sb = new StringBuilder();
+
+            int byteAmount = int.Parse(sr.ReadLine());
+
+            int longCount = byteAmount / 4;
+
+
+            for(int i=0;i<longCount;i++)
+            {
+                sb.Append("long ");
+            }
+            sb.Append("int");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 20260212_beakjoon_16112/20260212_beakjoon_16112/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20260212_beakjoon_16112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+            string[] input = sr.ReadLine().Split(' ');
+            string[] input_nd = sr.ReadLine().Split(' ');
+
+            int acount = int.Parse(input[0]);
+            int activeCount = int.Parse(input[1]);
+
+            List<int> stone = new List<int>();
+
+            for (int i = 0; i < acount; i++)
+            {
+                stone.Add(int.Parse(input_nd[i]));
+            }
+
+            stone.Sort();
+
+            int result = 0;
+
+            for(int i= stone.Count-1; activeCount>=0; i--)
+            {
+                result += stone[i] * activeCount;
+                activeCount--;
+            }
+
+            Console.WriteLine($"{result}");
+            Console.ReadLine();
+        }
+    }
+}

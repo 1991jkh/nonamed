cec758ce816d1d3eecb279f6afef08a5f095f062
1991jkh <demonicjkh@gmail.com>
2026-02-22 00:34:54 +0900

    give up

--- 20260220_beakjoon_34892/20260220_beakjoon_34892/Program.cs
+++ 20260220_beakjoon_34892/20260220_beakjoon_34892/Program.cs
@@ -12,38 +12,57 @@ namespace _20260220_beakjoon_34892
         static void Main(string[] args)
         {
             StreamReader sr = new StreamReader(Console.OpenStandardInput());
-            StringBuilder sb = new StringBuilder();
 
             string[] input = sr.ReadLine().Split(' ');
 
-            int n = int.Parse(input[0]);
-            int a = int.Parse(input[1]);
-            int b = int.Parse(input[2]);
-            int c = int.Parse(input[3]);
-            int d = int.Parse(input[4]);
-            int e = int.Parse(input[5]);
-            int f = int.Parse(input[6]);
-            int g = int.Parse(input[7]);
-            int h = int.Parse(input[8]);
+            long n = long.Parse(input[0]);
+            long a = long.Parse(input[1]);
+            long b = long.Parse(input[2]);
+            long c = long.Parse(input[3]);
+            long d = long.Parse(input[4]);
+            long e = long.Parse(input[5]);
+            long f = long.Parse(input[6]);
+            long g = long.Parse(input[7]);
+            long h = long.Parse(input[8]);
 
-            if(((a-c)/(e-g) == (b-c)/(f-g)) && ((b-c)/(f-g) != (d-c*n)/(h-g*n)))
+            if ((a - c) * (f - g) == (b - c) * (e - g))
             {
-                Console.WriteLine("2");
-            }
-            else if(((a-c)/(e-g) == (b-c)/(f-g)) && ((b-c)/(f-g) == (d-c*n)/(h-g*n)))
-            {
-                Console.WriteLine("1");
+                if ((b - c) * (h - g * n) == (f - g) * (d - c * n) && (a - c) * (h - g * n) == (e - g) * (d - c * n))
+                {
+                    Console.WriteLine("1");
+                }
+                else
+                {
+                    Console.WriteLine("2");
+                }
             }
             else
             {
-                int x = (((h - g*n) * (b - c)) - ((f - g) * (d - c*n))) / (((e - g) * (b - c)) - ((f - g) * (a - c)));
-                int y = (((d - c*n) - (a - c)) * x) / (b - c);
-                int z = n-x-y;
+                if ((((f - g) * (d - c * n)) - ((h - g * n) * (b - c))) % (((a - c) * (f - g)) - ((b - c) * (e - g))) == 0 &&
+                    (((a - c) * (h - g * n)) - ((e - g) * (d - c * n))) % (((a - c) * (f - g)) - ((b - c) * (e - g))) == 0)
+                {
+                    long x = (((f - g) * (d - c * n)) - ((h - g * n) * (b - c))) / (((a - c) * (f - g)) - ((b - c) * (e - g)));
+                    long y = (((a - c) * (h - g * n)) - ((e - g) * (d - c * n))) / (((a - c) * (f - g)) - ((b - c) * (e - g)));
+                    long z = n - x - y;
 
-                Console.WriteLine("0");
-                Console.WriteLine($"{x} {y} {z}");
+                    if (x >= 0 && y >= 0 && z >= 0)
+                    {
+                        Console.WriteLine("0");
+                        Console.WriteLine($"{x} {y} {z}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("2");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("2");
+                }
             }
             Console.ReadLine();
+            //반례 4 1 2 10 23 2 4 20 46
+            //기울기가 같은데 범위 안의 정수인 해는 1,1,2 하나라서
         }
     }
 }

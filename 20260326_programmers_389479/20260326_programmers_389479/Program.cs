a90a875e7bd375b951208f348e528ce1c134f5d0
1991jkh <demonicjkh@gmail.com>
2026-03-26 17:50:00 +0900

    1

---
+++ 20260326_programmers_389479/20260326_programmers_389479/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20260326_programmers_389479
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] players, int m, int k)
+        {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
